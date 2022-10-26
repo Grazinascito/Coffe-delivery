867345c7ae04f1bf5ed7998a774c26b624168749
Grazinascito <grazi.social84@gmail.com>
2022-10-26 20:24:47 -0300

    feat: add header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,22 @@
-import { Wrapper } from "./Styles";
+import { Wrapper, Location, Cart, WrapRightInfo } from "./Styles";
+import logo from "../../assets/logo.svg";
+import map_pin_fill from "../../assets/icons/map-pin-fill.svg";
+import shopping_cart_fill from "../../assets/icons/shopping-cart-fill.svg";
 
 export const Header = () => {
   return (
-    <>
-      <Wrapper>blabaala</Wrapper>
-      <p>alsdkasdsa</p>
-    </>
+    <Wrapper>
+      <img src={logo} alt="" />
+
+      <WrapRightInfo>
+        <Location>
+          <img src={map_pin_fill} alt="" />
+          Porto Alegre, RS
+        </Location>
+        <Cart>
+          <img src={shopping_cart_fill} alt="" />
+        </Cart>
+      </WrapRightInfo>
+    </Wrapper>
   );
 };
