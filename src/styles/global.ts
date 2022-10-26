be8a6571f00e6d912f49af4a4471f001a8eda92a
Grazinascito <grazi.social84@gmail.com>
2022-10-25 21:34:54 -0300

    feat(styles): add stitches

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,1 +1,18 @@
+import { createStitches } from "@stitches/react";
 
+createStitches({
+  theme: {
+    colors: {
+      purpleDark: "#4B2995",
+      purple: "#8047F8",
+      purpleLight: "#EBE5F9",
+      YellowDark: "#C47F17",
+      Yellow: "#DBAC2C",
+      YellowLight: "#F1E9C9",
+    },
+
+    fontSizes: {
+        
+    },
+  },
+});
