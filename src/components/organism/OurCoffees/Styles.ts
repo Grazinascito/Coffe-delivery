56f87ce320b51ca6ee071ae1d57c76a0b4bf1376
grazinascito <grazi.social84@gmail.com>
2023-02-04 20:47:29 -0300

    refactor: split the app

--- src/components/organism/OurCoffees/Styles.ts
+++
@@ -1,5 +0,0 @@
-import { styled } from "@stitches/react";
-
-export const Wrapper = styled("main", {
-  padding: "3.2rem 16rem",
-});
--- src/styles/global.ts
+++
@@ -1,7 +0,0 @@
-import { globalCss } from "@stitches/react";
-
-export const globalStyles = globalCss({
-  "*": { margin: 0, padding: 0, boxSizing: "border-box" },
-  html: { fontSize: "62.5%" },
-  body: { fontSize: "1.6rem", height: "100vh" },
-});
